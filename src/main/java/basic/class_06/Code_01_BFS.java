4834b12e255aba10d6717341433acf33951daa38
zouzhiye <zouzhiye@didachuxing.com>
2020-03-30 18:52:05 +0800

    添加算法描述

--- src/main/java/basic/class_06/Code_01_BFS.java
+++ src/main/java/basic/class_06/Code_01_BFS.java
@@ -4,26 +4,39 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * 广度优先遍历图的方式，是以一种类似波纹扩散的方式进行的，不断放大辐射半径，进而覆盖整张图。
+ * <p>
+ * 队列实现
+ * 1.选择起始顶点放入队列，并标记为已访问；
+ * 2.当队列不为空时，从队列中取出顶点作为目标顶点，将目标顶点的所有相邻且未被访问过的顶点放入队列，并标记为已访问；
+ * 3.重复执行步骤 2。
+ * <p>
+ * 1.利用队列实现
+ * 2.从源节点开始依次按照宽度进队列，然后弹出
+ * 3.每弹出一个点，把该节点所有没有进过队列的邻接点放入队列
+ * 4.直到队列为空
+ */
 public class Code_01_BFS {
 
-	public static void bfs(Node node) {
-		if (node == null) {
-			return;
-		}
-		Queue<Node> queue = new LinkedList<>();
-		HashSet<Node> map = new HashSet<>();
-		queue.add(node);
-		map.add(node);
-		while (!queue.isEmpty()) {
-			Node cur = queue.poll();
-			System.out.println(cur.value);
-			for (Node next : cur.nexts) {
-				if (!map.contains(next)) {
-					map.add(next);
-					queue.add(next);
-				}
-			}
-		}
-	}
+    public static void bfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        HashSet<Node> map = new HashSet<>();
+        queue.add(node);
+        map.add(node);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            System.out.println(cur.value);
+            for (Node next : cur.nexts) {
+                if (!map.contains(next)) {
+                    map.add(next);
+                    queue.add(next);
+                }
+            }
+        }
+    }
 
 }
--- src/main/java/basic/class_06/Code_02_DFS.java
+++ src/main/java/basic/class_06/Code_02_DFS.java
@@ -3,29 +3,37 @@ package basic.class_06;
 import java.util.HashSet;
 import java.util.Stack;
 
+/**
+ * 相对于广度优先访问，深度优先的方式更像是一条路走到黑，走不下去了再回到上个路口选择另外一条路。
+ * <p>
+ * 1.利用栈实现
+ * 2.从源节点开始把节点按照深度放入栈，然后弹出
+ * 3.每弹出一个点，把该节点及下一个没有进过站的邻接点放入栈
+ * 4.直到栈变空
+ */
 public class Code_02_DFS {
 
-	public static void dfs(Node node) {
-		if (node == null) {
-			return;
-		}
-		Stack<Node> stack = new Stack<>();
-		HashSet<Node> set = new HashSet<>();
-		stack.add(node);
-		set.add(node);
-		System.out.println(node.value);
-		while (!stack.isEmpty()) {
-			Node cur = stack.pop();
-			for (Node next : cur.nexts) {
-				if (!set.contains(next)) {
-					stack.push(cur);
-					stack.push(next);
-					set.add(next);
-					System.out.println(next.value);
-					break;
-				}
-			}
-		}
-	}
+    public static void dfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        HashSet<Node> set = new HashSet<>();
+        stack.add(node);
+        set.add(node);
+        System.out.println(node.value);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    System.out.println(next.value);
+                    break;
+                }
+            }
+        }
+    }
 
 }
